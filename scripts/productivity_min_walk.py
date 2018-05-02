c1bd374a60d7096bf2c5225d0e4d32aa294a6e57
Syed Hussain Ather <shussainather@gmail.com>
2018-05-01 21:18:50 -0400

    new normal distribution

--- scripts/productivity_min_walk.py
+++ scripts/productivity_min_walk.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 import statsmodels.api as sm
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
 
 productive_min = pd.DataFrame.from_csv("../csv/productive_min.csv", index_col="date")
 steps = pd.DataFrame.from_csv("../csv/steps_daily.csv", index_col="date")
@@ -14,15 +15,27 @@ z = anxiety["value"]
 
 #pearR = np.corrcoef(x,y)[1,0]
 
-w = np.poly1d(np.polyfit(x, y, 3))
-xp = np.linspace(0, 10, len(x))
+#w = np.poly1d(np.polyfit(x, y, 4))
 
+# Fit a normal distribution
+mu = 8000
+sigma = 3000
+x_axis = np.arange(min(x), max(x), 1000)
+
+# Label the plot
 fig = plt.figure(1)
 plt.title("Minutes of productivity vs. km walked")
 plt.xlabel("kilometers walked")
 plt.ylabel("Minutes spent on productive tasks on computer")
+
+# Plot the data
 plt.scatter(x, y)
-plt.plot(x, w(x), "b")
+#plt.plot(x, w(x), "b")
+
+# Plot the normal distribution
+plt.plot(x_axis, norm.pdf(x_axis,mu,sigma))
+
+# Add a legend, save, and close
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = OrderedDict(zip(labels, handles))
 plt.legend(by_label.values(), by_label.keys(), loc=4)
@@ -30,16 +43,25 @@ plt.savefig("../images/productivity_min_vs_km_walked.png")
 plt.show()
 plt.close()
 
+# Label the plot
 fig = plt.figure(1)
 plt.title("Minutes of productivity vs. km walked")
 plt.xlabel("kilometers walked")
 plt.ylabel("Minutes spent on productive tasks on computer")
+
+# Plot the data
+# Add red dots for days with anxiety tagged, blue dots otherwise
 for index, value in enumerate(x):
     if z.iloc[index] == 1:
         plt.scatter(value, y.iloc[index], c= "r", label="Anxiety tagged")
     else:
         plt.scatter(value, y.iloc[index], c="b", label="Anxiety not tagged")
-plt.plot(x, w(x), "b")
+
+# Plot the normal distribution
+#plt.plot(x, w(x), "b")
+plt.plot(x_axis, norm.pdf(x_axis,mu,sigma))
+
+# Add a legend, save, and close
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = OrderedDict(zip(labels, handles))
 plt.legend(by_label.values(), by_label.keys(), loc=4)
