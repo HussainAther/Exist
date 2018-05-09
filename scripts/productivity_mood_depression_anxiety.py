43da89c2e2a992608ef3f95e0afaa96f7567bbf4
Syed Hussain Ather <shussainather@gmail.com>
2018-05-09 16:30:01 -0400

    legend

--- scripts/productivity_mood_depression_anxiety.py
+++ scripts/productivity_mood_depression_anxiety.py
@@ -31,24 +31,58 @@ for i in depression["value"]:
 # Create a dictionary of mood colors
 mood_dict = {1: "#e50000", 2: "#f97306", 3: "#ffff14", 4: "#15b01a", 5 : "#0343df"}
 
-# Plot the figure of productivity over time with days of anxiety and depression and mood colors.
+# Plot the figure of productivity over time with days of depression and mood colors.
 fig = plt.figure()
 ax = fig.add_subplot(111)
 for index, value in enumerate(mood["value"]):
-    plt.bar(index, productive_min.iloc[index], color=mood_dict[int(mood.iloc[index])])
-ax.set_title('Productivity over time by mood with days of depression and anxiety')
+    plt.bar(index, productive_min.iloc[index], color=mood_dict[int(mood.iloc[index])], label=int(mood.iloc[index]))
+ax.set_title('Productivity over time by mood with days of depression')
 ax.set_xlabel('Day')
 ax.set_ylabel('Productive minutes')
 
 # Add the labels
 rects = ax.patches
-ax.set_xticklabels(anxiety_labels)
 
 for rect, label in zip(rects, depression_labels):
     height = rect.get_height()
     ax.text(rect.get_x() + rect.get_width() / 2, height + 5, label,
             ha='center', va='bottom')
-plt.savefig("../images/productivity_mood_depression_anxiety.png")
+
+# Add legend
+handles, labels = ax.get_legend_handles_labels()
+labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+by_label = OrderedDict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys(), loc=4)
+
+plt.savefig("../images/productivity_mood_depression.png")
+plt.show()
+plt.close()
+
+# Plot the figure of productivity over time with days of anxiety and mood colors.
+fig = plt.figure()
+ax = fig.add_subplot(111)
+for index, value in enumerate(mood["value"]):
+    plt.bar(index, productive_min.iloc[index], color=mood_dict[int(mood.iloc[index])], label=int(mood.iloc[index]))
+ax.set_title('Productivity over time by mood with days of anxiety')
+ax.set_xlabel('Day')
+ax.set_ylabel('Productive minutes')
+
+# Add the labels
+rects = ax.patches
+#ax.set_xticklabels(anxiety_labels)
+
+for rect, label in zip(rects, anxiety_labels):
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2, height + 5, label,
+            ha='center', va='bottom')
+
+# Add legend
+handles, labels = ax.get_legend_handles_labels()
+labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+by_label = OrderedDict(zip(labels, handles))
+plt.legend(by_label.values(), by_label.keys(), loc=4)
+
+plt.savefig("../images/productivity_mood_anxiety.png")
 plt.show()
 plt.close()
 
@@ -56,7 +90,7 @@ plt.close()
 ax = fig.add_subplot(111)
 plt.figure(figsize=(12, 8))
 ax = productive_min["value"].plot(kind='bar')
-ax.set_title('Productivity over time with days of depression and anxiety')
+ax.set_title('Productivity over time with days of depression and depression')
 ax.set_xlabel('Day')
 ax.set_ylabel('Productive minutes')
 
@@ -69,6 +103,7 @@ for rect, label in zip(rects, depression_labels):
     height = rect.get_height()
     ax.text(rect.get_x() + rect.get_width() / 2, height + 5, label,
             ha='center', va='bottom')
+
 plt.savefig("../images/productivity_depression_anxiety.png")
 plt.show()
 plt.close()
